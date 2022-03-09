3e57603ded34eb20e0d809389ae9462c8baca11c
stewart-prysm <stew.bracken@gmail.com>
2022-03-09 14:48:45 -0800

    twitter

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,12 @@ export default function Document() {
         <Html>
             <Head>
                 {/*<meta name="viewport" content="width=device-width, initial-scale=1"/>*/}
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:site" content="@stewbracken" />
+                <meta name="twitter:creator" content="@stewbracken" />
+                <meta property="og:title" content="Hyperion" />
+                <meta property="og:description" content="Opening a portal to the next chapter of what NFTs are, Hyperion is generative art of a completely unique nature." />
+                <meta property="og:image" content="https://www.hyperionnft.art/twitter-share.jpg" />
 
                 <meta
                     name="description"
@@ -16,6 +22,7 @@ export default function Document() {
                 
                         <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap" rel="stylesheet" /> 
 
+                
             </Head>
             <body>
             <Main/>
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
-
 const configBase = {
   // Testnet values
   testnet: {
@@ -17,18 +16,18 @@ const configBase = {
     fileUrlThumbnails: "https://files.endala.xyz/testnet/thumbnails", // I am hosting cached versions on S3 because its much faster
     fileUrlMetadata: "https://files.endala.xyz/testnet/metadata",
     fileUrlRarities: "https://files.endala.xyz/testnet/rarities",
-    testnet:true,
+    testnet: true,
   },
   // Production Values
   production: {
-    rpcEndpoint: "https://rpc-stargaze.keplr.app",
-    restEndpoint: "https://lcd-stargaze.keplr.app",
+    rpcEndpoint: "https://rpc.stargaze-apis.com/",
+    restEndpoint: "",
     chainId: "stargaze-1",
     // Custom values here
     sg721: "stars1msfmkx4xmts3el3r46c2u2eehxkhahqzsa0crn0v58cncp7ztr7qrshkkh", // UPDATE ME to your production contract
     minter: "stars1944la2whlrumpc80nhghr75djrz8pmk3ddufktp9087spssq2rwqnp57my", // UPDATE ME to your production contract
-    mintPriceStars: 200, // UPDATE ME to Your Mint Price
-    totalNumMints: 641, // UPDATE ME to Your total mints
+    mintPriceStars: 350, // UPDATE ME to Your Mint Price
+    totalNumMints: 1024, // UPDATE ME to Your total mints
     thumbFiletype: ".jpg", // UPDATE ME to Your total mints
     metadataType: ".json", // UPDATE ME to Your total mints
     rarityType: ".json", // UPDATE ME to Your total mints
@@ -36,7 +35,7 @@ const configBase = {
     fileUrlThumbnails: "https://files.endala.xyz/testnet/thumbnails",
     fileUrlMetadata: "https://files.endala.xyz/testnet/metadata",
     fileUrlRarities: "https://files.endala.xyz/testnet/rarities",
-    testnet:false,
+    testnet: false,
   },
   useTestnet: true, // Set to false on production
 };
